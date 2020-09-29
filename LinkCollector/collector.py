b60de6c688324affafee9d86db6e84ddd33212c1
Brian Barreto <62407713+brbarreto0821@users.noreply.github.com>
2020-09-29 14:04:16 -0400

    Update collector.py

--- LinkCollector/collector.py
+++ LinkCollector/collector.py
@@ -1,139 +1,47 @@
-import abc
-import uuid
+from urllib.request import urlopen
+from urllib.parse import urlparse
+import re
+import sys
 
-class IntroToPython:
-    def lesson(self):
-        return f"""
-            Hello {self.student}. Define two variables,
-            an integer named a with value 1
-            and a string named b with value 'hello'
-            
-        """
-    
-    def check(self, code):
-        return code == "a = 1\nb = 'hello'"
-    
-# abstract base class
-class Assignment(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def lesson(self, student):
-        pass
-    
-    @abc.abstractmethod
-    def check(self, code):
-        pass
-    
-    @classmethod
-    def __subclasshook__(cls, C):
-        if cls is Assignment:
-            attrs = set(dir(C))
-            if set(cls.__abstractmethods__) <= attrs:
-                return True
-        
-        return NotImplemented
+LINK_REGEX = re.compile(
+        "<a [^>]*href=['\"]([^'\"]+)['\"][^>]*>")
 
-class Statistics(Assignment):
-    def lesson(self):
-        return (
-            "Good work so far, "
-            + self.student
-            + ". Now calculate the average of the numbers "
-            + " 1, 5, 18, -3 and assign to a variable named 'avg'"
-        )
-    
-    def check(self, code):
-        import statistics
+class LinkCollector:
+    def __init__(self, url):
+        self.url = "http://%s" % urlparse(url).netloc
+        self.collected_links = {}
+        self.visited_links = set()
         
-        code = "import statistics\n" + code
+    def collect_links(self, path='/'):
+        full_url = self.url + path
+        self.visited_links.add(full_url)
+        page = str(urlopen(full_url).read())
+        links = LINK_REGEX.findall(page)
+        links = {self.normalize_url(path, link
+                                   ) for link in links}
+        self.collected_links[full_url] = links
         
-        local_vars = {}
-        global_vars = {}
-        exec(code, global_vars, local_vars)
+        for link in links:
+            self.collected_links.setdefault(link, set())
         
-        return local_vars.get("avg") == statistics.mean([1, 5, 18, -3])
-    
-class AssignmentGrader:
-    def __init__(self, student, AssignmentClass):
-        self.assignment = AssignmentClass()
-        self.assignment.student = student
-        self.attempts = 0
-        self.correct_attempts = 0
+        unvisited_links = links.difference(
+                self.visited_links)
         
-    def check(self, code):
-        self.attempts += 1
-        result = self.assignment.check(code)
-        if result:
-            self.correct_attempts += 1
+        for link in unvisited_links:
+            if link.startswith(self.url):
+                self.collect_links(urlparse(link).path)
+                
+    def normalize_url(self, path, link):
+        if link.startswith("http://"):
+            return link
+        elif link.startswith("/"):
+            return self.url + link
+        else:
+            return self.url + path.rpartition(
+                '/')[0] + '/' + link
         
-        return result
-    
-    def lesson(self):
-        return self.assignment.lesson()
-
-class Grader:
-    def __init__(self):
-        self.student_graders = {}
-        self.assignment_classes = {}
-        
-    def register(self, assignment_class):
-        if not issubclass(assignment_class, Assignment):
-            raise RuntimeError(
-                "Your class does not have the right methods"
-            )
-        
-        id = uuid.uuid4()
-        self.assignment_classes[id] = assignment_class
-        return id
-    
-    def start_assignment(self, student, id):
-        self.student_graders[student] = AssignmentGrader(
-            student, self.assignment_classes[id]
-        )
-    
-    def get_lesson(self, student):
-        assignment = self.student_graders[student]
-        return assignment.lesson()
-    
-    def check_assignment(self, student, code):
-        assignment = self.student_graders[student]
-        return assignment.check(code)
-    
-    def assignment_summary(self, student):
-        grader = self.student_graders[student]
-        return f"""
-        {student}'s attempts at {grader.assignment.__class__.__name__}:
-        
-        attempts: {grader.attempts}
-        correct: {grader.correct_attempts}
-        passed: {grader.correct_attempts > 0}
-        """
-
-# test code
-grader = Grader()
-itp_id = grader.register(IntroToPython)
-stat_id = grader.register(Statistics)
-
-grader.start_assignment("Brian", itp_id)
-print("Brian's Lesson:", grader.get_lesson("Brian"))
-print(
-    "Brian's check:",
-    grader.check_assignment("Brian", "a = 1\nb = 'hello'"),
-)
-print(
-    "Brian's other check:",
-    grader.check_assignment("Brian", "a = 1\nb = 'hello'"),
-)
-
-print(grader.assignment_summary("Brian"))
-
-grader.start_assignment("Brian", stat_id)
-print("Brian's Lesson:", grader.get_lesson("Brian"))
-print("Brian's check:", grader.check_assignment("Brian", "avg=5.25"))
-print(
-    "Brian's other check:",
-    grader.check_assignment(
-        "Brian", "avg = statistics.mean([1, 5, 18, -3])"
-    ),
-)
-
-print(grader.assignment_summary("Brian"))
+if __name__ == "__main__":
+    collector = LinkCollector('http://localhost:8000')
+    collector.collect_links()
+    for link, item in collector.collected_links.items():
+        print("{}: {}".format(link, item))
